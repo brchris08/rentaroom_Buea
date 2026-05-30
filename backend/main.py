0be9daf5b7b7823a109bc2d5e7e8c59102156825
Bake Chris <chris08bake@gmail.com>
2026-05-30 08:22:24 +0100

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -27,7 +27,7 @@ app.mount("/static", StaticFiles(directory="frontend/static"), name="static")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
